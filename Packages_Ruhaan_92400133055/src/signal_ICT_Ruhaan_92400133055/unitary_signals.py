34b309b562402ed9ba3d16662a24a1888bccd09a
Ruhaan_Pathan <139214773+ruhaanpathan@users.noreply.github.com>
2025-09-06 12:24:22 +0530

    Unitary_File comp

--- Packages_Ruhaan_92400133055/src/signal_ICT_Ruhaan_92400133055/unitary_signals.py
+++ Packages_Ruhaan_92400133055/src/signal_ICT_Ruhaan_92400133055/unitary_signals.py
@@ -1,2 +1,42 @@
-def Hello():
-    print("Hii")
+import numpy as np
+import matplotlib.pyplot as mat
+
+def unit_step(n):
+    half = n // 2
+    n_vec = np.arange(-half, -half + n)
+    u = (n_vec >= 0).astype(int)
+    mat.figure()
+    mat.stem(n_vec, u,basefmt=" ")
+    mat.title(f"Unit Step (length={n})")
+    mat.xlabel("n")
+    mat.ylabel("u[n]")
+    mat.grid(True)
+    mat.show()
+    return u
+
+def unit_impulse(n):
+
+    half = n // 2
+    n_vec = np.arange(-half, -half + n)
+    delta = (n_vec == 0).astype(int)
+    mat.figure()
+    mat.stem(n_vec, delta, basefmt=" ")
+    mat.title(f"Unit Impulse (length={n})")
+    mat.xlabel("n")
+    mat.ylabel("delta[n]")
+    mat.grid(True)
+    mat.show()
+    return delta
+
+def ramp_signal(n):
+    half = n // 2
+    n_vec = np.arange(-half, -half + n)
+    ramp = np.where(n_vec >= 0, n_vec, 0).astype(float)
+    mat.figure()
+    mat.stem(n_vec, ramp, basefmt=" ")
+    mat.title(f"Ramp Signal (length={n})")
+    mat.xlabel("n")
+    mat.ylabel("r[n]")
+    mat.grid(True)
+    mat.show()
+    return ramp
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import Packages_Ruhaan_92400133055.src.signal_ICT_Ruhaan_92400133055.unitary_signals as Unitary_signals
+import Packages_Ruhaan_92400133055.src.signal_ICT_Ruhaan_92400133055.unitary_signals as Unitary_signals 
 
+Unitary_signals.unit_step(20)
+Unitary_signals.ramp_signal(20)
+Unitary_signals.unit_impulse(20)
 
-
-Unitary_signals.Hello()
